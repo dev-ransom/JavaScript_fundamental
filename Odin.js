c4055eaf4b8466e231f975ef734c76fb063a7772
devransom-github <sundaystanley56@gmail.com>
2022-12-02 06:16:50 +0100

    add updates

--- Odin.js
+++ Odin.js
@@ -9,3 +9,10 @@ console.log(stn);
 
 var message = "hello";
 alert(message);
+var hello = "hello world"
+var message = hello;
+console.log(message);
+let userName = 'herryayo';
+console.log(userName);
+let $ = 4;
+console.log($);
