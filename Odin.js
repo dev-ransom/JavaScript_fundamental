b46e422f32d699fa57791f69715ec6ed64eeb791
devransom-github <sundaystanley56@gmail.com>
2022-12-03 08:06:21 +0100

    add updates

--- Odin.js
+++ Odin.js
@@ -1,10 +1,4 @@
-const COLOR_RED = '#F00';
-const COLOR_BLUE = '#0F00';
-const COLOR_GREEN = '#F0000';
-const COLOR_ORANGE = '#F000';
-
-let color = COLOR_ORANGE;
-let colour = COLOR_GREEN;
-alert(colour)
- 
-
+let name, admin;
+name = "John";
+admin = name ;
+alert(admin)
