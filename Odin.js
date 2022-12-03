c6e0cca86e5c3738413f93b8da18614f06d2f9b4
devransom-github <sundaystanley56@gmail.com>
2022-12-03 07:13:27 +0100

    add updates

--- Odin.js
+++ Odin.js
@@ -1,5 +1,10 @@
+const COLOR_RED = '#F00';
+const COLOR_BLUE = '#0F00';
+const COLOR_GREEN = '#F0000';
+const COLOR_ORANGE = '#F000';
 
-const myBirthday = '25.01.2002';
-const myBirthday = '35.02.3008'
+let color = COLOR_ORANGE;
+let colour = COLOR_GREEN;
+alert(colour)
  
 
